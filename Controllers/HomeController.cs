6959a1f9ef6595c57fbdec90a8483942fbcd6ee3
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2024-07-11 22:00:25 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pratice.Models;
+using System.Diagnostics;
+using Pratice.Models;
+namespace Pratice.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Book> bookList = new List<Book>();
+            bookList.Add(new Book("Lẽ Nào Em Không Biết", 622200, "th (1).jpg", "Robbert", "ccc"));
+            bookList.Add(new Book("Em là nhất", 55000, "th (2).jpg", "Robbert", "ccc"));
+            bookList.Add(new Book("Khuôn viên ngôn từ", 71250, "th (3).jpg", "Robbert", "ccc"));
+            bookList.Add(new Book("Người truyền ký ức", 49640, "th (4).jpg", "Robbert", "ccc"));
+            bookList.Add(new Book("Đạo tình", 50000, "th (5).jpg", "Robbert", "ccc"));
+            bookList.Add(new Book("Yêu", 30000, "th (6).jpg", "Robbert", "ccc"));
+            bookList.Add(new Book("Kỳ án ánh trăng", 115000, "th (7).jpg", "Robbert", "ccc"));
+            bookList.Add(new Book("Cuộc sống không giới hạn", 74000, "th (8).jpg", "Robbert", "ccc"));
+            return View(bookList);
+        }
+        public IActionResult Item(int index)
+        {
+            List<Book> bookList = new List<Book>();
+            bookList.Add(new Book("Lẽ Nào Em Không Biết", 622200, "th (1).jpg", "Robbert", "ccc"));
+            bookList.Add(new Book("Em là nhất", 55000, "th (2).jpg", "Robbert", "ccc"));
+            bookList.Add(new Book("Khuôn viên ngôn từ", 71250, "th (3).jpg", "Robbert", "ccc"));
+            bookList.Add(new Book("Người truyền ký ức", 49640, "th (4).jpg", "Robbert", "ccc"));
+            bookList.Add(new Book("Đạo tình", 50000, "th (5).jpg", "Robbert", "ccc"));
+            bookList.Add(new Book("Yêu", 30000, "th (6).jpg", "Robbert", "ccc"));
+            bookList.Add(new Book("Kỳ án ánh trăng", 115000, "th (7).jpg", "Robbert", "ccc"));
+            bookList.Add(new Book("Cuộc sống không giới hạn", 74000, "th (8).jpg", "Robbert", "ccc"));
+            ViewBag.id = bookList[index];
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pratice.Models
+{
+    public class Book
+    {
+        string name, link, author, description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        int cost;
+
+        public string Link
+        {
+            get { return link; }
+            set { link = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Cost
+        {
+            get { return cost; }
+            set { cost = value; }
+        }
+        public Book(string name, int cost, string link, string author, string description)
+        {
+            this.name = name;
+            this.cost = cost;
+            this.link = link;
+            this.author = author;
+            this.description = description;
+        }
+        public Book()
+        {
+            name = "Sách 1 ";
+            cost = 100000;
+            link = "OIP(1).jpg";
+            author = "Robbert";
+            description = "aaaa";
+        }
+    }
+}
